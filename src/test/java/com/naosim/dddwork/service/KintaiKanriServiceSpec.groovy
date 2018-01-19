3e612269c7b9a6532a6a36cfcfc77aae045f365e
Keisuke Nakamura <ke-nakamura@cnt.biglobe.co.jp>
2018-01-19 13:21:48 +0900

    メインロジックをserviceに移動し、serviceのテストコード追加

---
+++ src/test/java/com/naosim/dddwork/service/KintaiKanriServiceSpec.groovy
@@ -0,0 +1,25 @@
+package com.naosim.dddwork.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+@ContextConfiguration(locations = ["classpath:context.xml"])
+class KintaiKanriServiceSpec extends Specification {
+
+    @Autowired
+    KintaiKanriService kintaiKanriService
+
+    def "inputでエラーが発生しないことを確認する"() {
+        setup:
+        // TODO: data.csvを削除したほうがいいかな
+
+        when:
+        String[] args = ["input", "20180102", "0900", "1830"]
+        kintaiKanriService.execute(args)
+
+        then:
+        true
+    }
+
+}
