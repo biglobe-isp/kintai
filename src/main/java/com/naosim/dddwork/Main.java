f63147be300a6b63a9d55c3739c8c8a35fbd1def
Keisuke Nakamura <ke-nakamura@cnt.biglobe.co.jp>
2018-01-22 10:28:29 +0900

    MainクラスからDIを使用してserviceを呼び出そうとしたが、現状エラーとなる（念のためコミットしておく）
    ・url-shortenerの構成がWebアプリを想定しているため、いろいろ設定を行う必要がある、もしくは不要な設定を削除する必要がある

--- src/main/java/com/naosim/dddwork/Main.java
+++ src/main/java/com/naosim/dddwork/Main.java
@@ -1,15 +1,30 @@
 package com.naosim.dddwork;
 
 import com.naosim.dddwork.service.KintaiKanriService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
+@Component
 public class Main {
+
+    @Autowired
+    private KintaiKanriService kintaiKanriService;
 
     public static void main(String[] args) {
         try {
-            KintaiKanriService service = new KintaiKanriService();
-            service.execute(args);
+            ApplicationContext app = new ClassPathXmlApplicationContext("classpath:context.xml");
+            Main main = app.getBean(Main.class);
+            main.execute(args);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void execute(String[] args) throws IOException {
+        this.kintaiKanriService.execute(args);
     }
 }
