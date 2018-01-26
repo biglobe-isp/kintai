979b5da530ecd536ebb401b33a5da23cb20b90cb
Keisuke Nakamura <ke-nakamura@cnt.biglobe.co.jp>
2018-01-26 10:15:57 +0900

    リファクタリング
    ・テストクラスのpackage整理中

--- src/test/java/com/naosim/dddwork/domain/kintai/time/TimeSpec.groovy
+++ src/test/java/com/naosim/dddwork/domain/kintai/time/TimeSpec.groovy
@@ -1,6 +1,5 @@
-package com.naosim.dddwork.domain
+package com.naosim.dddwork.domain.kintai.time
 
-import com.naosim.dddwork.domain.kintai.time.Time
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
