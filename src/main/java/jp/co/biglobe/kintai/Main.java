b018ff73a853ca172e4a487335535032c718f746
shigerujinnai <s-jinnai@cnt.biglobe.co.jp>
2018-09-13 15:01:03 +0900

    可読性向上のための定数化

--- src/main/java/jp/co/biglobe/kintai/Main.java
+++ src/main/java/jp/co/biglobe/kintai/Main.java
@@ -5,27 +5,39 @@ import jp.co.biglobe.kintai.domain.KintaiManager;
 import java.time.LocalDateTime;
 
 public class Main {
+
+    private static String MethodType_Input = "input";
+    private static String MethodType_Total = "total";
+
+    private static int InputArgsIndex_MethodType = 0;
+    private static int InputArgsIndex_Date = 1;
+    private static int InputArgsIndex_StartTime = 2;
+    private static int InputArgsIndex_EndTime = 3;
+
+    private static int InputArgsIndex_YearMonth = 1;
+
     public static void main(String[] args) {
         try {
             if (args.length < 1) {
                 throw new RuntimeException("引数が足りません");
             }
-            String methodType = args[0];
             KintaiManager manager = new KintaiManager();
 
-            if ("input".equals(methodType)) {
+            String methodType = args[InputArgsIndex_MethodType];
+
+            if (MethodType_Input.equals(methodType)) {
                 if (args.length < 4) {
                     throw new RuntimeException("引数が足りません");
                 }
-                String date = args[1];
-                String start = args[2];
-                String end = args[3];
+                String date = args[InputArgsIndex_Date];
+                String start = args[InputArgsIndex_StartTime];
+                String end = args[InputArgsIndex_EndTime];
                 String now = LocalDateTime.now().toString();
 
                 manager.input(date, start, end, now);
 
-            } else if ("total".equals(methodType)) {
-                String yearMonth = args[1];
+            } else if (MethodType_Total.equals(methodType)) {
+                String yearMonth = args[InputArgsIndex_YearMonth];
                 if (args.length < 2) {
                     throw new RuntimeException("引数が足りません");
                 }
