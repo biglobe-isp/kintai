04a2a1f34bbb85ea4188ed66331c33940fe156f9
shigerujinnai <s-jinnai@cnt.biglobe.co.jp>
2018-09-13 13:57:49 +0900

    不要なimport文の除去

--- src/main/java/jp/co/biglobe/kintai/Main.java
+++ src/main/java/jp/co/biglobe/kintai/Main.java
@@ -2,14 +2,7 @@ package jp.co.biglobe.kintai;
 
 import jp.co.biglobe.kintai.domain.KintaiManager;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
