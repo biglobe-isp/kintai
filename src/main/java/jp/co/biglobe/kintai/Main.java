6b663aaeed79b6dc47cdbd9d91da7255f13ac58c
shigerujinnai <s-jinnai@cnt.biglobe.co.jp>
2018-09-13 13:55:20 +0900

    input,total処理をMainより移行

--- src/main/java/jp/co/biglobe/kintai/Main.java
+++ src/main/java/jp/co/biglobe/kintai/Main.java
@@ -1,5 +1,7 @@
 package jp.co.biglobe.kintai;
 
+import jp.co.biglobe.kintai.domain.KintaiManager;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -12,13 +14,14 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         try {
-            if(args.length < 1) {
+            if (args.length < 1) {
                 throw new RuntimeException("引数が足りません");
             }
             String methodType = args[0];
+            KintaiManager manager = new KintaiManager();
 
-            if("input".equals(methodType)) {
-                if(args.length < 4) {
+            if ("input".equals(methodType)) {
+                if (args.length < 4) {
                     throw new RuntimeException("引数が足りません");
                 }
                 String date = args[1];
@@ -26,81 +29,20 @@ public class Main {
                 String end = args[3];
                 String now = LocalDateTime.now().toString();
 
-                int startH = Integer.valueOf(start.substring(0, 2));
-                int startM = Integer.valueOf(start.substring(2, 4));
+                manager.input(date, start, end, now);
 
-                int endH = Integer.valueOf(end.substring(0, 2));
-                int endM = Integer.valueOf(end.substring(2, 4));
-
-                int workMinutes = endH * 60 + endM - (startH * 60 + startM);
-
-                if(endH == 12) {
-                    workMinutes -= endM;
-                } else if(endH >= 13) {
-                    workMinutes -= 60;
-                }
-
-                if(endH == 18) {
-                    workMinutes -= endM;
-                } else if(endH >= 19) {
-                    workMinutes -= 60;
-                }
-
-                if(endH == 21) {
-                    workMinutes -= endM;
-                } else if(endH >= 22) {
-                    workMinutes -= 60;
-                }
-
-                int overWorkMinutes = Math.max(workMinutes - 8 * 60, 0);
-                File file = new File("data.csv");
-                try(FileWriter filewriter = new FileWriter(file, true)) {
-                    filewriter.write(String.format("%s,%s,%s,%s,%s,%s\n", date, start, end, workMinutes, overWorkMinutes, now));
-                }
-
-            } else if("total".equals(methodType)) {
+            } else if ("total".equals(methodType)) {
                 String yearMonth = args[1];
-                if(args.length < 2) {
+                if (args.length < 2) {
                     throw new RuntimeException("引数が足りません");
                 }
 
-                int totalWorkMinutes = 0;
-                int totalOverWorkMinutes = 0;
-
-                File file = new File("data.csv");
-
-                try(
-                        FileReader fr = new FileReader(file);
-                        BufferedReader br = new BufferedReader(fr);
-                ) {
-
-                    String line = br.readLine();
-                    Map<String, Integer> totalWorkMinutesMap = new HashMap<>();
-                    Map<String, Integer> totalOverWorkMinutesMap = new HashMap<>();
-                    while(line != null){
-                        String[] columns = line.split(",");
-                        if(!columns[0].startsWith(yearMonth)) {
-                            continue;
-                        }
-                        totalWorkMinutesMap.put(columns[0], Integer.valueOf(columns[3]));
-                        totalOverWorkMinutesMap.put(columns[0], Integer.valueOf(columns[4]));
-
-                        line = br.readLine();
-                    }
-
-                    Set<String> keySet = totalWorkMinutesMap.keySet();
-                    for(String key : keySet) {
-                        totalWorkMinutes += totalWorkMinutesMap.get(key);
-                        totalOverWorkMinutes += totalOverWorkMinutesMap.get(key);
-                    }
-
-                    System.out.println("勤務時間: " + totalWorkMinutes / 60 + "時間" + totalWorkMinutes % 60 + "分");
-                    System.out.println("残業時間: " + totalOverWorkMinutes / 60 + "時間" + totalOverWorkMinutes % 60 + "分");
-                }
+                manager.total(yearMonth);
 
             } else {
                 throw new RuntimeException("methodTypeが不正です");
             }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
--- src/main/java/jp/co/biglobe/kintai/domain/KintaiManager.java
+++ src/main/java/jp/co/biglobe/kintai/domain/KintaiManager.java
@@ -1,4 +1,87 @@
 package jp.co.biglobe.kintai.domain;
 
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class KintaiManager {
+
+    public void input(String date, String start, String end, String now){
+        int startH = Integer.valueOf(start.substring(0, 2));
+        int startM = Integer.valueOf(start.substring(2, 4));
+
+        int endH = Integer.valueOf(end.substring(0, 2));
+        int endM = Integer.valueOf(end.substring(2, 4));
+
+        int workMinutes = endH * 60 + endM - (startH * 60 + startM);
+
+        if(endH == 12) {
+            workMinutes -= endM;
+        } else if(endH >= 13) {
+            workMinutes -= 60;
+        }
+
+        if(endH == 18) {
+            workMinutes -= endM;
+        } else if(endH >= 19) {
+            workMinutes -= 60;
+        }
+
+        if(endH == 21) {
+            workMinutes -= endM;
+        } else if(endH >= 22) {
+            workMinutes -= 60;
+        }
+
+        int overWorkMinutes = Math.max(workMinutes - 8 * 60, 0);
+        File file = new File("data.csv");
+        try(FileWriter filewriter = new FileWriter(file, true)) {
+            filewriter.write(String.format("%s,%s,%s,%s,%s,%s\n", date, start, end, workMinutes, overWorkMinutes, now));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void total(String yearMonth){
+
+        int totalWorkMinutes = 0;
+        int totalOverWorkMinutes = 0;
+
+        File file = new File("data.csv");
+
+        try(
+                FileReader fr = new FileReader(file);
+                BufferedReader br = new BufferedReader(fr);
+        ) {
+
+            String line = br.readLine();
+            Map<String, Integer> totalWorkMinutesMap = new HashMap<>();
+            Map<String, Integer> totalOverWorkMinutesMap = new HashMap<>();
+            while(line != null){
+                String[] columns = line.split(",");
+                if(!columns[0].startsWith(yearMonth)) {
+                    continue;
+                }
+                totalWorkMinutesMap.put(columns[0], Integer.valueOf(columns[3]));
+                totalOverWorkMinutesMap.put(columns[0], Integer.valueOf(columns[4]));
+
+                line = br.readLine();
+            }
+
+            Set<String> keySet = totalWorkMinutesMap.keySet();
+            for(String key : keySet) {
+                totalWorkMinutes += totalWorkMinutesMap.get(key);
+                totalOverWorkMinutes += totalOverWorkMinutesMap.get(key);
+            }
+
+            System.out.println("勤務時間: " + totalWorkMinutes / 60 + "時間" + totalWorkMinutes % 60 + "分");
+            System.out.println("残業時間: " + totalOverWorkMinutes / 60 + "時間" + totalOverWorkMinutes % 60 + "分");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+}
 }
