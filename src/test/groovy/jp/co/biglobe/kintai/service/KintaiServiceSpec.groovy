136ac61af6a13c26bda319394c2ce5baa79b5001
shigerujinnai <s-jinnai@cnt.biglobe.co.jp>
2018-10-01 17:07:06 +0900

    勤怠サービスInput処理の呼び出しテスト作成

--- src/test/groovy/jp/co/biglobe/kintai/service/KintaiServiceSpec.groovy
+++ src/test/groovy/jp/co/biglobe/kintai/service/KintaiServiceSpec.groovy
@@ -1,12 +1,21 @@
 package jp.co.biglobe.kintai.service
 
+import jp.co.biglobe.kintai.domain.WorkTimeRepository
 import spock.lang.Specification
 import spock.lang.Unroll
 
 @Unroll
 class KintaiServiceSpec extends Specification {
 
-    def "サービスのテスト"(){
+    def "input呼び出しのテスト"(){
+        setup:
+        WorkTimeRepository repository = Mock()
+        KintaiService service = new KintaiService(repository)
 
+        when:
+        service.input("20181001","0900","1800","1500")
+
+        then:
+        1 * repository.input(_)
     }
 }
