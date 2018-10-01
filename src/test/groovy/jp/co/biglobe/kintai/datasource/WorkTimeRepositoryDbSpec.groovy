3ec6fd65815fa9deb8af2b6c4c6fad109c027e3d
shigerujinnai <s-jinnai@cnt.biglobe.co.jp>
2018-10-01 18:05:16 +0900

    勤怠リポジトリクラスInput処理のテスト作成

--- src/test/groovy/jp/co/biglobe/kintai/datasource/WorkTimeRepositoryDbSpec.groovy
+++ src/test/groovy/jp/co/biglobe/kintai/datasource/WorkTimeRepositoryDbSpec.groovy
@@ -1,5 +1,8 @@
 package jp.co.biglobe.kintai.datasource
 
+import jp.co.biglobe.kintai.domain.EndWorkTime
+import jp.co.biglobe.kintai.domain.StartWorkTime
+import jp.co.biglobe.kintai.domain.WorkDate
 import jp.co.biglobe.kintai.domain.WorkTime
 import spock.lang.Specification
 import spock.lang.Unroll
@@ -7,8 +10,25 @@ import spock.lang.Unroll
 @Unroll
 class WorkTimeRepositoryDbSpec extends Specification{
 
-    def "リポジトリテスト"(){
+    private def workTimeRepositoryDb = new WorkTimeRepositoryDb()
+
+    private def startTime = new StartWorkTime("0900")
+    private def endTime = new EndWorkTime("1800")
+    private def workDate  = new WorkDate("20181010")
+
+    def "inputの処理テスト"(){
+        setup:
+        def workTime = new WorkTime()
+        workTime.setDate(workDate)
+        workTime.setStartTime(startTime)
+        workTime.setEndTime(endTime)
+        workTime.setNow("now")
+
+
+        when:
+        workTimeRepositoryDb.input(workTime)
+
+        then:
+        true
     }
-
-
 }
