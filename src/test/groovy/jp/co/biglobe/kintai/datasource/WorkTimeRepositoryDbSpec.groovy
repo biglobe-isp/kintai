fe9e687c99166403154f3becd76e12ace3e14c8e
shigerujinnai <s-jinnai@cnt.biglobe.co.jp>
2018-10-01 16:04:53 +0900

    テストクラスの作成

---
+++ src/test/groovy/jp/co/biglobe/kintai/datasource/WorkTimeRepositoryDbSpec.groovy
@@ -0,0 +1,14 @@
+package jp.co.biglobe.kintai.datasource
+
+import jp.co.biglobe.kintai.domain.WorkTime
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@Unroll
+class WorkTimeRepositoryDbSpec extends Specification{
+
+    def "リポジトリテスト"(){
+    }
+
+
+}
---
+++ src/test/groovy/jp/co/biglobe/kintai/service/KintaiServiceSpec.groovy
@@ -0,0 +1,12 @@
+package jp.co.biglobe.kintai.service
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@Unroll
+class KintaiServiceSpec extends Specification {
+
+    def "サービスのテスト"(){
+
+    }
+}
