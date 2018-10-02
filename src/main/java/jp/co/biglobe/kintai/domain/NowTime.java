cb58cb9e144287dd3edba401c6b12e37252fb269
shigerujinnai <s-jinnai@cnt.biglobe.co.jp>
2018-10-02 15:02:17 +0900

    現時刻を保持したバリューオブジェクトを作成

---
+++ src/main/java/jp/co/biglobe/kintai/domain/NowTime.java
@@ -0,0 +1,14 @@
+package jp.co.biglobe.kintai.domain;
+
+public class NowTime {
+
+    private final String now;
+
+    public NowTime(String now){
+        this.now = now;
+    }
+
+    public String getNow() {
+        return now;
+    }
+}
