535d43779ecbb96ed7d349c8cb4569a1bc3969b1
shigerujinnai <s-jinnai@cnt.biglobe.co.jp>
2018-10-04 11:00:39 +0900

    勤務時間Builderを作成

---
+++ src/main/java/jp/co/biglobe/kintai/domain/WorkTimeBuilder.java
@@ -0,0 +1,18 @@
+package jp.co.biglobe.kintai.domain;
+
+public final class WorkTimeBuilder {
+
+    public WorkTimeBuilder() {
+    }
+
+    public WorkTime buildWorkTime(WorkTime workTime, int minutes, int overWorkMinutes) {
+        return new WorkTime(
+                workTime.getDate(),
+                workTime.getStartTime(),
+                workTime.getEndTime(),
+                minutes,
+                overWorkMinutes,
+                workTime.getNow()
+        );
+    }
+}
