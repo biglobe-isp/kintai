edcf93043a99e01da272ad662106ca01eb357b3c
shigerujinnai <s-jinnai@cnt.biglobe.co.jp>
2018-10-10 16:47:12 +0900

    就業規則のテストを修正

--- src/test/groovy/jp/co/biglobe/kintai/domain/WorkingRuleSpec.groovy
+++ src/test/groovy/jp/co/biglobe/kintai/domain/WorkingRuleSpec.groovy
@@ -12,18 +12,19 @@ class WorkingRuleSpec extends Specification {
         def workDate = new WorkDate(date)
         def startWorkTime = new StartWorkTime(start)
         def endWorkTime = new EndWorkTime(end)
-        def workTime = WorkingRule.getInstance().calculateWorkTime(
-                workDate, startWorkTime, endWorkTime, "なう")
+        def nowTime = new NowTime(now)
+        def time = new WorkTime(workDate,startWorkTime,endWorkTime,0,0,nowTime)
+        def workTime = WorkingRule.getInstance().calculateWorkTime(time)
 
         expect:
         workTime.minutes == ex_minites
 
         where:
-        date|start|end|ex_minites
-        "200001888"|"0900"|"1800"|480
-        "200001888"|"0900"|"2000"|540
-        "200001888"|"0900"|"2400"|720
-        "2000040000000"|"0900"|"2000"|540
+        date|start|end|now|ex_minites
+        "200001888"|"0900"|"1800"|"20181003"|480
+        "200001888"|"0900"|"2000"|"20181003"|540
+        "200001888"|"0900"|"2400"|"20181003"|720
+        "2000040000000"|"0900"|"2000"|"20181003"|540
 
    }
 }
