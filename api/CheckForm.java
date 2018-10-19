ab4c93226f74ac42588ae3622dcda0f8f2011861
fukushimaayano <a-fukushima@za.biglobe.co.jp>
2018-10-19 15:06:16 +0900

    機能追加

--- api/CheckForm.java
+++ api/CheckForm.java
@@ -16,16 +16,24 @@ public class CheckForm {
         ChangeForm changeForm = new ChangeForm();
 
         if ("input".equals(form[0])) {
-            if (form.length < 4) {
+            if (form.length < 3) {
                 throw new RuntimeException("引数が足りません");
+            } else if ("v".equals(form[2])) {
+                this.start = changeForm.getStart("-start:0900");
+                this.end = changeForm.getEnd("-end:1800");
+            } else if ("am".equals(form[2])) {
+                this.start = changeForm.getStart("-start:1200");
+                this.end = changeForm.getEnd(form[3]);
+            } else if ("pm".equals(form[3])) {
+                this.start = changeForm.getStart(form[2]);
+                this.end = changeForm.getEnd("-end:1200");
+            } else {
+                this.start = changeForm.getStart(form[2]);
+                this.end = changeForm.getEnd(form[3]);
             }
-
             this.methodType = MethodType.INPUT;
             this.date = changeForm.getDate(form[1]);
-            this.start = changeForm.getStart(form[2]);
-            this.end = changeForm.getEnd(form[3]);
             this.work = new WorkMinutesDomain(start, end, date);
-
 
         } else if ("total".equals(form[0])) {
             if (form.length < 2) {
