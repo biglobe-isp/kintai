57cd96f860959b11d15c542a5f64b0a2192d8915
shigerujinnai <s-jinnai@cnt.biglobe.co.jp>
2018-10-30 09:32:38 +0900

    テストケースのインデント整理及びリファクタリング

--- src/test/groovy/jp/co/biglobe/kintai/domain/WorkTimeCardBuilderSpec.groovy
+++ src/test/groovy/jp/co/biglobe/kintai/domain/WorkTimeCardBuilderSpec.groovy
@@ -3,10 +3,10 @@ package jp.co.biglobe.kintai.domain
 import spock.lang.Specification
 import spock.lang.Unroll
 
-@Unroll
 class WorkTimeCardBuilderSpec extends Specification{
 
 
+    @Unroll
     def "タイムカード統計処理のテストgetMonthlyWorkTimCard"(){
 
         setup:
@@ -15,15 +15,15 @@ class WorkTimeCardBuilderSpec extends Specification{
         when:
         sut.punch("20181001",workTime)
         sut.punch("20181002",workTime2)
-        Optional<MonthlyWorkTimeCard> actual = sut.getMonthlyWorkTimCard()
+        Optional<MonthlyWorkTimeCard> actual = sut.build()
 
         then:
         actual == Optional.of(expected)
 
         where:
-        workTime|workTime2||expected
-        new WorkTime(100,200)|new WorkTime(300,400)||new MonthlyWorkTimeCard(400,600)
-        new WorkTime(300,400)|new WorkTime(500,600)||new MonthlyWorkTimeCard(800,1000)
+        workTime                |workTime2               ||expected
+        new DailyReport(100,200)|new DailyReport(300,400)||new MonthlyWorkTimeCard(400,600)
+        new DailyReport(300,400)|new DailyReport(500,600)||new MonthlyWorkTimeCard(800,1000)
 
     }
 
