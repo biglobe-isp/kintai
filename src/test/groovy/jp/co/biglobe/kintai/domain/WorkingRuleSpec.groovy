dfc296bddf790f5214971490f913f0568fa7a7b6
shigerujinnai <s-jinnai@cnt.biglobe.co.jp>
2018-11-01 09:14:51 +0900

    就業規則テストの修正

--- src/test/groovy/jp/co/biglobe/kintai/domain/WorkingRuleSpec.groovy
+++ src/test/groovy/jp/co/biglobe/kintai/domain/WorkingRuleSpec.groovy
@@ -12,10 +12,10 @@ class WorkingRuleSpec extends Specification {
 
     def "就業規則による休憩時間を加味した勤務時間の算出:#testcase"() {
         setup:
-        def workDate = new WorkDate(date)
+        def workDate = new WorkDate("20181018")
         def startWorkTime = new StartWorkTime(start)
         def endWorkTime = new EndWorkTime(end)
-        def nowTime = new NowTime(now)
+        def nowTime = new NowTime("20181003")
         def time = new WorkTime(workDate, startWorkTime, endWorkTime, 0, 0, nowTime)
         def breakTime1 = new BreakTime(new Time("1200"), new Time("1300"))
         def breakTime2 = new BreakTime(new Time("1800"), new Time("1900"))
@@ -24,17 +24,17 @@ class WorkingRuleSpec extends Specification {
         breakTimes.add(breakTime1)
         breakTimes.add(breakTime2)
         breakTimes.add(breakTime3)
+
         def workTime = WorkingRule.getInstance().calculateWorkTime(time, breakTimes)
 
         expect:
         workTime.minutes == ex_minites
 
         where:
-        testcase | date            | start  | end    | now        | ex_minites
-        "成功"     | "200001888"     | "0900" | "1800" | "20181003" | 480
-        "午後"     | "200001888"     | "1200" | "2000" | "20181003" | 360
-        "1日"     | "200001888"     | "0900" | "2400" | "20181003" | 720
-        "日付異常値"  | "2000040000000" | "0900" | "2000" | "20181003" | 540
+        testcase | start  | end    | ex_minites
+        "成功"     | "0900" | "1800" | 480
+        "午後"     | "1200" | "2000" | 360
+        "1日"     | "0900" | "2400" | 720
 
     }
 }
