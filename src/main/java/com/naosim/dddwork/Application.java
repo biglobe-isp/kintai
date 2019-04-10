cffbab4bdf363bb6c7ff4488e526af5b63792767
d-sudo <d-sudo@cnt.biglobe.co.jp>
2019-04-10 16:36:36 +0900

    Convert to Spring Boot Application

---
+++ src/main/java/com/naosim/dddwork/Application.java
@@ -0,0 +1,22 @@
+package com.naosim.dddwork;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.setLogStartupInfo(false);
+        springApplication.run(args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Main.main(args);
+    }
+}
