214557d77caad37f4119d5ef19103d6672c896e2
a-shinozaki <a-shinozaki@cnt.biglobe.co.jp>
2019-05-15 14:02:00 +0900

    fix: 永続化csvのフォーマットが変わってしまっていたのを修正
    * MessageFormat の pattern 指定に半角スペースを入れていてそれが影響していた

--- src/main/java/com/naosim/dddwork/kintai/domain/core/type/time/component/TimeOfDay.java
+++ src/main/java/com/naosim/dddwork/kintai/domain/core/type/time/component/TimeOfDay.java
@@ -43,7 +43,74 @@ public class TimeOfDay {
 
     public String defaultStoredValue() {
 
-        MessageFormat mf = new MessageFormat("{0, number, 00}{1, number, 00}");
+        MessageFormat mf = new MessageFormat("{0,number,00}{1,number,00}");
         return mf.format(new Integer[] {hour.value, minute.value});
     }
+
+
+//    int comparableAmount() {
+//        return hour.comparableAmount() + minute.comparableAmount();
+//    }
+//
+//    public boolean isBefore(TimeOfDay other) {
+//
+//        final int thisAmount = comparableAmount();
+//        final int otherAmount = other.comparableAmount();
+//
+//        return thisAmount < otherAmount;
+////        return asLocalTime.isBefore(time.asLocalTime);
+//    }
+//
+//    public boolean isAfter(TimeOfDay other) {
+//
+//        final int thisAmount = comparableAmount();
+//        final int otherAmount = other.comparableAmount();
+//
+//        return thisAmount > otherAmount;
+////        return asLocalTime.isAfter(time.asLocalTime);
+//    }
+//
+//    public long until(TimeOfDay time, ChronoUnit unit) {
+//        return asLocalTime.until(time.asLocalTime, unit);
+//    }
+//
+//
+//
+//    public static TimeOfDay findMinimum(TimeOfDay... evaluationTargets) {
+//
+//        return Arrays.stream(evaluationTargets).reduce(
+//                TimeOfDay.ofMaximumInDay(),
+//                (minCandidate, target) -> (target.isBefore(minCandidate)) ? target : minCandidate
+//        );
+//
+////        TimeOfDay minCandidate = TimeOfDay.findMaximum();
+////
+////        for (TimeOfDay timeOfDay: evaluationTargets) {
+////
+////            if (timeOfDay.isBefore(minCandidate)) {
+////                minCandidate = timeOfDay;
+////            }
+////        }
+////
+////        return minCandidate;
+//    }
+//
+//    public static TimeOfDay findMaximum(TimeOfDay... evaluationTargets) {
+//
+//        return Arrays.stream(evaluationTargets).reduce(
+//                TimeOfDay.ofMinimumInDay(),
+//                (maxCandidate, target) -> (target.isAfter(maxCandidate)) ? target : maxCandidate
+//        );
+//
+////        TimeOfDay minCandidate = TimeOfDay.findMininum();
+////
+////        for (TimeOfDay timeOfDay: evaluationTargets) {
+////
+////            if (timeOfDay.isAfter(minCandidate)) {
+////                minCandidate = timeOfDay;
+////            }
+////        }
+////
+////        return minCandidate;
+//    }
 }
