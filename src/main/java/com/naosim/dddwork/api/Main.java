527c008ad0b4657af9271aa891230b2a97700848
h-irie <h-irie@biglobe.co.jp>
2019-10-11 14:54:05 +0900

    登録時の入力形式を変更。
    例：
    input -date:YYYYMMDD -start:HH_MM -end:HH_MM

--- src/main/java/com/naosim/dddwork/api/Main.java
+++ src/main/java/com/naosim/dddwork/api/Main.java
@@ -19,10 +19,13 @@ public class Main {
             if (args.length < 4) {
                 throw new RuntimeException("引数が足りません");
             }
+            input(args);
+            /*
             LocalDate date = createLocalDate(args[1]);//YYYYMMDD
             LocalTime start = createLocalTime(args[2]);//HHMM
             LocalTime end = createLocalTime(args[3]);//HHMM
             KintaiService.input(date, start, end);
+             */
 
 
         } else if ("total".equals(methodType)) {
@@ -59,6 +62,49 @@ public class Main {
         int year = Integer.valueOf(yyyymm.substring(0, 4));
         int month = Integer.valueOf(yyyymm.substring(4, 6));
         return YearMonth.of(year, month);
+    }
+
+    private static void input(String[] args) {
+
+        LocalDate date = null;//YYYYMMDD
+        LocalTime start = null;//HHMM
+        LocalTime end = null;//HHMM
+
+        for (String agr : args) {
+            String[] str = agr.split(":");
+            switch (str[0]) {
+                case "-date":
+                    date = createLocalDate(extractDate(str));
+                    break;
+                case "-start":
+                    start = createLocalTime(extractTime(str));
+                    break;
+                case "-end":
+                    end = createLocalTime(extractTime(str));
+                    break;
+                default:
+
+            }
+        }
+        if (date == null || start == null || end == null) {
+            throw new IllegalArgumentException("引数の数が足りません。");
+        }
+        KintaiService.input(date, start, end);
+    }
+
+    private static String extractDate(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException();
+        }
+        return args[1];
+    }
+
+    private static String extractTime(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException();
+
+        }
+        return args[1].replaceAll("_", "");
     }
 
 }
