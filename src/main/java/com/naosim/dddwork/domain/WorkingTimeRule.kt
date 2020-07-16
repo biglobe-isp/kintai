84b794c7b446defe8f68ee978657da83e77057b7
y-kikuchi-biglobe <y-kikuchi@biglobe.co.jp>
2020-07-16 11:01:23 +0900

    勤務規程クラスに規程を使用した各種判定メソッドを追加

--- src/main/java/com/naosim/dddwork/domain/WorkingTimeRule.kt
+++ src/main/java/com/naosim/dddwork/domain/WorkingTimeRule.kt
@@ -9,4 +9,22 @@ data class WorkingTimeRule(
         val lunchBreakBorder: Time = Time.of(Hour(12), Minute(0)),
         val nightBreakBorder: Time = Time.of(Hour(18), Minute(0)),
         val midnightBreakBorder: Time = Time.of(Hour(21), Minute(0))
-)
+) {
+    companion object {
+        val FIXED_REST_TIME: WorkingTimeRange.RestTimeSpan = WorkingTimeRange.RestTimeSpan(Duration.ofHours(1L))
+    }
+
+    fun evalPastLunchBreakOnPunchedOut(punchedOutTime: PunchOutTime): Boolean = punchedOutTime >= lunchBreakBorder + FIXED_REST_TIME
+    fun evalPastNightBreakOnPunchedOut(punchedOutTime: PunchOutTime): Boolean = punchedOutTime >= nightBreakBorder + FIXED_REST_TIME
+    fun evalPastMidnightBreakOnPunchedOut(punchedOutTime: PunchOutTime): Boolean = punchedOutTime >= midnightBreakBorder + FIXED_REST_TIME
+    fun evalBreakTimeOnPunchedOut(punchedOutTime: PunchOutTime): Boolean = punchedOutTime.hour == lunchBreakBorder.hour
+            || punchedOutTime.hour == nightBreakBorder.hour
+            || punchedOutTime.hour == midnightBreakBorder.hour
+
+    fun bindToScheduledWorkingTimeSpan(workingTimeSpan: TimeSpan): WorkingTimeRange.ScheduledWorkingTimeSpan =
+            WorkingTimeRange.ScheduledWorkingTimeSpan.of(if (workingTimeSpan > scheduledWorkingTimeSpan) {
+                scheduledWorkingTimeSpan
+            } else {
+                workingTimeSpan
+            })
+}
