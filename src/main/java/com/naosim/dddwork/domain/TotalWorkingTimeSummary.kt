c88fe49bcf11f0952e82ff016e1633eb5fa149c0
y-kikuchi-biglobe <y-kikuchi@biglobe.co.jp>
2020-08-14 16:17:37 +0900

    出勤記録の集計処理をドメインに凝集する

--- src/main/java/com/naosim/dddwork/domain/TotalWorkingTimeSummary.kt
+++ src/main/java/com/naosim/dddwork/domain/TotalWorkingTimeSummary.kt
@@ -3,4 +3,14 @@ package com.naosim.dddwork.domain
 data class TotalWorkingTimeSummary (
         val totalWorkingTimeSpan: WorkingTimeRange.ActualWorkingTimeSpan,
         val totalExtraWorkingTimeSpan: WorkingTimeRange.ExtraWorkingTimeSpan
-)
+) {
+    companion object {
+        fun of(workingTimeRecords: List<WorkingTimeRecord>): TotalWorkingTimeSummary = workingTimeRecords
+                .fold(TotalWorkingTimeSummary(WorkingTimeRange.ActualWorkingTimeSpan.ZERO, WorkingTimeRange.ExtraWorkingTimeSpan.ZERO)) { acc, it ->
+                    TotalWorkingTimeSummary(
+                            totalWorkingTimeSpan = acc.totalWorkingTimeSpan + it.workingTimeRange.actualWorkingTimeSpan,
+                            totalExtraWorkingTimeSpan = acc.totalExtraWorkingTimeSpan + it.workingTimeRange.extraWorkingTimeSpan
+                    )
+                }
+    }
+}
---
+++ src/main/java/com/naosim/dddwork/service/WorkingTimePunchService.kt
@@ -0,0 +1,27 @@
+package com.naosim.dddwork.service
+
+import com.naosim.dddwork.domain.*
+import com.naosim.dddwork.domain.vo.*
+import java.time.LocalDateTime
+
+class WorkingTimeManagementService(
+        private val workingTimeRepository: WorkingTimeRepository,
+        private val workingTimeRule: WorkingTimeRule = WorkingTimeRule()
+) {
+    fun punch(workingDate: WorkingDate, punchInTime: PunchInTime, punchOutTime: PunchOutTime) {
+        workingTimeRepository.save(WorkingTimeRecord(
+                workingDate = workingDate,
+                // 本来は勤怠記録時に規程は必要ないが、今回の保存ファイルの仕様上ここで渡す必要がある
+                workingTimeRange = workingTimeRule.calcWorkingTimeRange(
+                        punchInTime = punchInTime,
+                        punchOutTime = punchOutTime
+                ),
+                punchedDateTime = PunchedDateTime(LocalDateTime.now())
+        ))
+    }
+
+    fun total(year: Year, month: Month): TotalWorkingTimeSummary =
+            workingTimeRepository.findByYearAndMonth(year, month, workingTimeRule).let {
+                TotalWorkingTimeSummary.of(it)
+            }
+}
