23b12e1363a0b6f594db69c900d33c0c61091828
K-Doi137 <k-doi@cnt.biglobe.co.jp>
2022-01-17 16:17:47 +0900

    仕様変更 入力方法変更

--- src/main/java/api/Main.java
+++ src/main/java/api/Main.java
@@ -11,9 +11,9 @@ public final class Main {
     private static final WorkRecordApplicationService WORK_RECORD_APPLICATION_SERVICE
             = new WorkRecordApplicationService(new CSVWorkRecordRepository());
 
-    private static final Pattern DATE_PATTERN = Pattern.compile("(?<year>\\d{4})(?<month>\\d{2})(?<day>\\d{2})");
-    private static final Pattern START_TIME_PATTERN = Pattern.compile("(?<hours>\\d{2})(?<minutes>\\d{2})");
-    private static final Pattern END_TIME_PATTERN = Pattern.compile("(?<hours>\\d{2})(?<minutes>\\d{2})");
+    private static final Pattern DATE_PATTERN = Pattern.compile("-date:(?<year>\\d{4})(?<month>\\d{2})(?<day>\\d{2})");
+    private static final Pattern START_TIME_PATTERN = Pattern.compile("-start:(?<hours>\\d{2})_(?<minutes>\\d{2})");
+    private static final Pattern END_TIME_PATTERN = Pattern.compile("-end:(?<hours>\\d{2})_(?<minutes>\\d{2})");
     private static final Pattern YEAR_MONTH_PATTERN = Pattern.compile("(?<year>\\d{4})(?<month>\\d{2})");
 
     public static void main(String[] args) {
@@ -57,7 +57,7 @@ public final class Main {
     private static int[] parseDate(String dateAsText) {
         Matcher dateMatcher = DATE_PATTERN.matcher(dateAsText);
         if (!dateMatcher.matches()) {
-            throw new IllegalArgumentException("年月日はYYYYMMDDの形式で入力してください。");
+            throw new IllegalArgumentException("年月日は-date:YYYYMMDDの形式で入力してください。");
         }
         int year = Integer.parseInt(dateMatcher.group("year"));
         int month = Integer.parseInt(dateMatcher.group("month"));
@@ -68,7 +68,7 @@ public final class Main {
     private static int[] parseStartTime(String startTimeAsText) {
         Matcher startTimeMatcher = START_TIME_PATTERN.matcher(startTimeAsText);
         if (!startTimeMatcher.matches()) {
-            throw new IllegalArgumentException("勤務開始時刻はhhmmの形式で入力してください。");
+            throw new IllegalArgumentException("勤務開始時刻は-start:hh_mmの形式で入力してください。");
         }
         int startHour = Integer.parseInt(startTimeMatcher.group("hours"));
         int startMinutes = Integer.parseInt(startTimeMatcher.group("minutes"));
@@ -78,7 +78,7 @@ public final class Main {
     private static int[] parseEndTime(String endTimeAsText) {
         Matcher endTimeMatcher = END_TIME_PATTERN.matcher(endTimeAsText);
         if (!endTimeMatcher.matches()) {
-            throw new IllegalArgumentException("勤務終了時刻はhhmmの形式で入力してください。");
+            throw new IllegalArgumentException("勤務終了時刻は-end:hh_mmの形式で入力してください。");
         }
         int endHour = Integer.parseInt(endTimeMatcher.group("hours"));
         int endMinutes = Integer.parseInt(endTimeMatcher.group("minutes"));
