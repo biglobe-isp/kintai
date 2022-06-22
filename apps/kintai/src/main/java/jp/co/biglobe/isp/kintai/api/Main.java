7e7238285a0575a457c173c0a6cf65421eaa0e8c
HirofumiShinkura <h-shinkura@biglobe.co.jp>
2022-06-22 18:16:59 +0900

    chore:optimize imports

--- apps/kintai/src/main/java/jp/co/biglobe/isp/kintai/api/Main.java
+++ apps/kintai/src/main/java/jp/co/biglobe/isp/kintai/api/Main.java
@@ -1,14 +1,5 @@
 package jp.co.biglobe.isp.kintai.api;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 public class Main {
     public static void main(String[] args) {
         try {
