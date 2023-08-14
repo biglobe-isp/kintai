83f7b9c5d648d99739e3eda4a3ebe97a09082913
satoshikawaura <135300637+satoshikawaura@users.noreply.github.com>
2023-08-14 16:35:31 +0900

    月合計の勤務、残業時間の計算ロジック追加

--- src/main/java/kintai/domain/MonthTotalWorkingTime.java
+++ src/main/java/kintai/domain/MonthTotalWorkingTime.java
@@ -1,19 +1,25 @@
 package kintai.domain;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 
 import java.util.List;
 
-@RequiredArgsConstructor
+@Value
 public class MonthTotalWorkingTime {
-    private final MonthTotalWorkMinutes monthTotalWorkMinutes;
-    private final MonthTotalOverWorkMinutes monthTotalOverWorkMinutes;
+    MonthTotalWorkMinutes monthTotalWorkMinutes;
+    MonthTotalOverWorkMinutes monthTotalOverWorkMinutes;
 
 
 
     public static MonthTotalWorkingTime totalTime(List<WorkingDateTotalRecord> workingDateTotalRecordList){
-
-        return new MonthTotalWorkingTime(new MonthTotalWorkMinutes(0),new MonthTotalOverWorkMinutes(0));
+        int totalWorkMinutes = 0;
+        int totalOverWorkMinutes = 0;
+        for(WorkingDateTotalRecord total : workingDateTotalRecordList){
+            totalWorkMinutes     += total.getWorkMinutes().getWorkMinutes();
+            totalOverWorkMinutes += total.getOverWorkMinutes().getOverWorkMinutes();
+        }
+        return new MonthTotalWorkingTime(new MonthTotalWorkMinutes(totalWorkMinutes),new MonthTotalOverWorkMinutes(totalOverWorkMinutes));
     }
 
 }
